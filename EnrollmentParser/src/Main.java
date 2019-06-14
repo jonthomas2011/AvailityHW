4fd0d54724c6d05eff8addc88fe420bcc0e86a83
Jonathan Thomas <jonthomas2011@gmail.com>
2019-06-13 21:20:11 -0400

    added functionality to create unique insurance files from .csv to desired/specified output from commandline input

--- EnrollmentParser/src/Main.java
+++ EnrollmentParser/src/Main.java
@@ -1,18 +1,25 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main (String args[]){
         File file = new File (args[0]);
+        String fileOutputPath = args[1];
         ArrayList<String> userID = new ArrayList<String>();
         ArrayList<String> name = new ArrayList<String>();
         ArrayList<String> version = new ArrayList<String>();
         ArrayList<String> insurance = new ArrayList<String>();
 
         parseFile(file,userID,name,version,insurance);
+        createFiles(insurance, fileOutputPath);
     }
 
     // reads the entire file and separates the columns into different ArrayLists
@@ -33,4 +40,19 @@ public class Main {
             insurance.add(reader.next());
         }
     }
+    public static void createFiles(ArrayList insurance, String fileOutputPath) {
+        List<String> distinctCompanies = (List<String>) insurance.stream().distinct().collect(Collectors.toList());
+
+        for (String company : distinctCompanies) {
+            File file = new File(fileOutputPath + company +".csv");
+            boolean fvar;
+            try {
+                if (file.createNewFile()) fvar = true;
+                else fvar = false;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
