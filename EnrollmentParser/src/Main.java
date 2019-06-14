f3468b8088f9c2d98b7b8ecb1372a0d99581cadf
Jonathan Thomas <jonthomas2011@gmail.com>
2019-06-13 23:56:10 -0400

    added functionality to reorder first and last name in lists as well as sort content in ascending order

--- EnrollmentParser/src/Main.java
+++ EnrollmentParser/src/Main.java
@@ -1,11 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -20,37 +16,69 @@ public class Main {
 
         parseFile(file,userID,name,version,insurance);
         createFiles(insurance, fileOutputPath);
+        ArrayList reorderedNames = reorderNames(name);
+        sortContent(userID,reorderedNames,version, insurance);
     }
 
     // reads the entire file and separates the columns into different ArrayLists
     public static void parseFile(File file, ArrayList userID, ArrayList name, ArrayList version, ArrayList insurance)
     {
+        int index = 0;
         Scanner reader = null;
         try {
             reader = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        reader.useDelimiter("[;\\r\\n]+");
+        reader.useDelimiter("[,\\r\\n]+");
         while(reader.hasNext())
         {
-            userID.add(reader.next());
-            name.add(reader.next());
-            version.add(reader.next());
-            insurance.add(reader.next());
+            userID.add(index,reader.next());
+            name.add(index,reader.next());
+            version.add(index, reader.next());
+            insurance.add(index, reader.next());
+            index++;
         }
     }
     public static void createFiles(ArrayList insurance, String fileOutputPath) {
         List<String> distinctCompanies = (List<String>) insurance.stream().distinct().collect(Collectors.toList());
 
         for (String company : distinctCompanies) {
-            File file = new File(fileOutputPath + company +".csv");
+            File file = new File(fileOutputPath + company +".txt");
             boolean fvar;
             try {
                 if (file.createNewFile()) fvar = true;
                 else fvar = false;
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+    public static ArrayList<String> reorderNames(ArrayList<String> name ){
+
+        ArrayList reordered = new ArrayList();
+        for (String n : name) {
+            String fName = n.substring(0,n.indexOf(" "));
+            String lName = n.substring(n.indexOf(" "));
+            reordered.add(lName + " " + fName);
+        }
+        return reordered;
+    }
+    public static void sortContent(ArrayList userID, ArrayList name, ArrayList version, ArrayList insurance) {
+
+        String[] content = new String[name.size()];
+
+        for (int i = 0; i < name.size(); i++){
+            content[i] = (String) name.get(i) + ',' + (String) userID.get(i) + ',' + (String) version.get(i) + ',' + (String) insurance.get(i);
+        }
+
+        for(int i = 0; i< name.size(); i++) {
+            for (int j = i+1; j<content.length; j++) {
+                if(content[i].compareTo(content[j])>0) {
+                    String temp = content[i];
+                    content[i] = content[j];
+                    content[j] = temp;
+                }
             }
         }
     }
